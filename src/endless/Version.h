ec287bb0e56845a2fcfc6bc36635d6b1cabe6746
Radu Ancuta <radu.ancuta@movial.com>
2016-06-02 17:42:48 +0300

    Increased version to 1.0.0.9 for release.

--- src/endless/Version.h
+++ src/endless/Version.h
@@ -3,7 +3,7 @@
 #define RELEASE_VER_MAIN    1 
 #define RELEASE_VER_MAIN2   0
 #define RELEASE_VER_SUB     0
-#define RELEASE_VER_SUB2    8
+#define RELEASE_VER_SUB2    9
 
 // version number (string)
 #define TOSTRING2(arg) #arg
